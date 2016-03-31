521799c41cfb6e44470ae204e54dd4f6a3fa19a9
Wilson Yu <wilsonweichengyu@gmail.com>
2016-03-31 16:00:59 -0700

    push to live

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
-    },
+
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['public/client/**/*.js'],
+        dest: 'public/dist/<%= pkg.name %>.js'
+      }
+          },
 
     mochaTest: {
       test: {
@@ -21,16 +29,40 @@ module.exports = function(grunt) {
     },
 
     uglify: {
-    },
+
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'public/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+          },
 
     eslint: {
       target: [
-        // Add list of files to lint here
-      ]
+
+        'Gruntfile.js',
+        'app/**/*.js',
+        'public/**/*.js',
+        'lib/**/*.js',
+        './*.js',
+        'spec/**/*.js'
+              ]
     },
 
     cssmin: {
-    },
+
+      options: {
+        keepSpecialComments: 0
+      },
+      dist: {
+        files: {
+          'public/dist/style.min.css': 'public/style.css'
+        }
+      }
+          },
 
     watch: {
       scripts: {
@@ -51,7 +83,14 @@ module.exports = function(grunt) {
 
     shell: {
       prodServer: {
-      }
+
+        command: 'git push live master',
+        options: {
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
+              }
     },
   });
 
@@ -80,8 +119,9 @@ module.exports = function(grunt) {
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
-      // add your production server task here
-    }
+
+      grunt.task.run([ 'shell:prodServer' ]);
+          }
     grunt.task.run([ 'server-dev' ]);
   });
 
@@ -90,23 +130,33 @@ module.exports = function(grunt) {
   ////////////////////////////////////////////////////
 
   grunt.registerTask('test', [
-    'mochaTest'
+
+    'eslint',
+        'mochaTest'
   ]);
 
   grunt.registerTask('build', [
-  ]);
+
+    'concat',
+    'uglify',
+    'cssmin'
+      ]);
 
   grunt.registerTask('upload', function(n) {
     if (grunt.option('prod')) {
-      // add your production server task here
-    } else {
+
+      grunt.task.run([ 'shell:prodServer' ]);
+          } else {
       grunt.task.run([ 'server-dev' ]);
     }
   });
 
   grunt.registerTask('deploy', [
-    // add your deploy tasks here
-  ]);
+
+    'test',
+    'build',
+    'upload'
+      ]);
 
 
 };
--- server-config.js
+++ server-config.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var partials = require('express-partials');
 var util = require('./lib/utility');
-
 var handler = require('./lib/request-handler');
 
 var app = express();
